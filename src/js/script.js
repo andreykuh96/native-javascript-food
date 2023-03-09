6f7c395ec59504da6ab26ffa36d0a0d66ea2e21c
Andrey <andrey.kuh96@gmail.com>
2023-03-09 15:25:22 +0300

    timer

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    //Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -34,4 +37,56 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
+
+    //Timer
+
+    function getTimeRemaining(endTime) {
+        const timeDifference = Date.parse(endTime) - Date.parse(new Date()),
+              days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((timeDifference / (1000 * 60)) % 60),
+              seconds = Math.floor((timeDifference / 1000) % 60);
+
+        return {
+            'total': timeDifference,
+            days,
+            hours,
+            minutes,
+            seconds
+        }
+    }
+
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function getZero(num) {
+            if (num > 0 && num < 10) {
+                return `0${num}`
+            } else {
+                return num;
+            }
+        }
+
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total < 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', '2023-05-20');
 });
